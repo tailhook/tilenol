d8dc4759dc96170ffdbc7aadf817b1f009d2e077
Paul Colomiets <paul@colomiets.name>
2012-03-04 23:49:08 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='Tilenol',
+      version='0.1',
+      description='Window manager written in python with greenlets',
+      author='Paul Colomiets',
+      author_email='paul@colomiets.name',
+      url='http://github.com/tailhook/tilenol',
+      classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        ],
+      packages=['tilenol'],
+    )
