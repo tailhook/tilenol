92fc21f17d59e1aad5e82b5348daab84abf9bf94
Paul Colomiets <paul@colomiets.name>
2012-05-15 00:22:41 +0300

    Better handling auth data, added utility to debug X auth

--- tilenol/xcb/auth.py
+++ tilenol/xcb/auth.py
@@ -12,5 +12,13 @@ def read_auth(filename=os.path.expanduser("~/.Xauthority")):
         return f.read(val)
     with open(filename, 'rb') as f:
         while True:
-            family, = struct.unpack('<H', f.read(2))
+            head = f.read(2)
+            if not head:
+                break
+            family, = struct.unpack('<H', head)
             yield Auth(family, rstr(), int(rstr()), rstr(), rstr())
+
+
+if __name__ == '__main__':
+    for item in read_auth():
+        print(item)
