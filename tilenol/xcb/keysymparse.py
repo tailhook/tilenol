3b71f6600d437a4e5f167315683e7f0137cd3788
Paul Colomiets <paul@colomiets.name>
2021-05-07 14:39:55 +0300

    Fix support of fresh keysym file

--- tilenol/xcb/keysymparse.py
+++ tilenol/xcb/keysymparse.py
@@ -1,7 +1,12 @@
 import os
 import re
+import logging
 
-keysym_re = re.compile(r"^#define\s+(XF86)?XK_(\w+)\s+(\S+)")
+log = logging.getLogger(__name__)
+keysym_re = re.compile(
+    r"^#define\s+(XF86)?XK_(\w+)\s+"
+    r"(?:(0x[a-fA-F0-9]+)|_EVDEV\((0x[0-9a-fA-F]+)\))"
+)
 
 
 class Keysyms(object):
@@ -18,7 +23,22 @@ class Keysyms(object):
                 if not m:
                     continue
                 name = (m.group(1) or '') + m.group(2)
-                code = int(m.group(3), 0)
+
+                if m.group(3):
+                    try:
+                        code = int(m.group(3), 0)
+                    except ValueError:
+                        log.warn("Bad code %r for key %r", code, name)
+                        continue
+                elif m.group(4):
+                    try:
+                        code = int(m.group(4), 0)
+                    except ValueError:
+                        log.warn("Bad code %r for evdev key %r", code, name)
+                        continue
+                else:
+                    continue
+
                 self.__dict__[name] = code
                 self.name_to_code[name] = code
                 self.code_to_name[code] = name
